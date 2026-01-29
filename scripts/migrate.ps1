5b292bd0b9ce5773391e72c9f831a8706bbc6135
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-29 18:42:14 +0530

    Update migrate.ps1

--- scripts/migrate.ps1
+++ scripts/migrate.ps1
@@ -3,3 +3,10 @@ $VM = "ubuntuserver"
 
 $vm = Get-AzVM -Name $VM -ResourceGroupName $RG
 Write-Host "VM found:" $vm.Name
+
+$nic = Get-AzNetworkInterface -ResourceId $vm.NetworkProfile.NetworkInterfaces[0].Id
+$pipId = $nic.IpConfigurations[0].PublicIpAddress.Id
+$pipName = ($pipId -split "/")[-1]
+
+$nic.IpConfigurations[0].PublicIpAddress = $null
+Set-AzNetworkInterface $nic
