12591a7196e40cf8ef71e1c2a7a1c24162baa45a
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-02-06 17:44:41 +0530

    Update phase0-export-backup.ps1

--- scripts/phase0-export-backup.ps1
+++ scripts/phase0-export-backup.ps1
@@ -5,7 +5,7 @@ Import-Module Az.RecoveryServices -Force
 
 # INPUT
 $SourceSubscriptionId = "46689057-be43-4229-9241-e0591dad4dbf"
-$VMName = "ubuntuServer"
+$VMName = "ubuntu"
 
 Set-AzContext -SubscriptionId $SourceSubscriptionId
 
