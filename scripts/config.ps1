eec52ca008d3c903193a53ef93a10985cf438623
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-02-09 17:17:50 +0530

    Create config.ps1

---
+++ scripts/config.ps1
@@ -0,0 +1,17 @@
+# ==========================================
+# CENTRAL CONFIG FILE
+# ==========================================
+
+# Source
+$SourceSubscriptionId      = "46689057-be43-4229-9241-e0591dad4dbf"
+$SourceResourceGroup       = "Dev-RG"
+
+# Destination
+$DestinationSubscriptionId = "d4e068bf-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+$DestinationResourceGroup  = "Prod-RG"
+$DestinationLocation       = "centralindia"
+
+# VM
+$VMName = "ubuntu"
+
+# ==========================================
