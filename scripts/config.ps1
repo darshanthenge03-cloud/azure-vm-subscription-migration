a1868a6bf8eeacdb6368a98a82f6e7aed64a01d1
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-02-09 17:33:52 +0530

    Update config.ps1

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -3,12 +3,12 @@
 # ==========================================
 
 # Source
-$SourceSubscriptionId      = "46689057-be43-4229-9241-e0591dad4dbf"
+$SourceSubscriptionId      = "d4e068bf-2473-4201-b10a-7f8501d50ebc"
 $SourceResourceGroup       = "Dev-RG"
 
 # Destination
-$DestinationSubscriptionId = "d4e068bf-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-$DestinationResourceGroup  = "Prod-RG"
+$DestinationSubscriptionId = "46689057-be43-4229-9241-e0591dad4dbf"
+$DestinationResourceGroup  = "Dev-RG"
 $DestinationLocation       = "centralindia"
 
 # VM
